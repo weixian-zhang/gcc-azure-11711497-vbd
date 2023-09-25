3b4e562c6e0ba78dea1b351f962dc93f3f6a3051
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2023-09-25 20:44:44 +0800

    feat(dsc): Add DSC config

---
+++ src/aamc-custom-registry-check/dsc-for-audit-registry.ps1
@@ -0,0 +1,47 @@
+
+
+Configuration RegistryAssertion
+{
+    # parameters will be override by policy definition as documented in below link
+    # https://learn.microsoft.com/en-us/azure/governance/machine-configuration/how-to-create-policy-definition#using-parameters-in-custom-machine-configuration-policy-definitions
+    param
+    (
+        [Parameter(Mandatory=$false)]
+        [PSCustomObject[]] $Registries =  @(
+            @{
+                Key     = "HKEY\2"
+                Value   = "1"
+                Ensure  = "Present"
+            },
+            @{
+                Key     = "HKEY\1"
+                Value   = "1"
+                Ensure  = "Present"
+            }
+        )
+    )
+
+    Install-Module PSDscResources
+   # Import-DscResource -ModuleName PSDscResources
+    #Import-DscResource -Name 'Registry' -ModuleName 'PSDscResources'
+    
+    $i = 0
+    Node localhost {
+        foreach ($reg in $Registries)
+        {       
+        
+    
+            Registry "RegistryAssertion_$($i.ToString())"
+            {
+                Ensure      = $reg.Ensure
+                Key         = $reg.Key
+                ValueName   = $reg.Value
+            }
+
+            $i += 1
+        }
+    }
+}
+
+RegistryAssertion
+
