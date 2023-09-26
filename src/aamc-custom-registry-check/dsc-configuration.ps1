bbc9ac83b5799d9204288bcaf00e4006d68f8bb0
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2023-09-26 20:40:46 +0800

    bug(policy-def): Avoid Json as param, instead per policy per registry check.
    Each DSC registry resourcd property is a separate policy definition parameter

--- src/aamc-custom-registry-check/dsc-configuration.ps1
+++ src/aamc-custom-registry-check/dsc-configuration.ps1
@@ -4,49 +4,28 @@ Configuration RegistryAssertion
 {
     # default parameters will be override by policy definition as documented in below link
     # https://learn.microsoft.com/en-us/azure/governance/machine-configuration/how-to-create-policy-definition#using-parameters-in-custom-machine-configuration-policy-definitions
-    param
-    (
-        [Parameter(Mandatory=$true)]
-        [String]
-        $Registries = '{    "registries": [        {"Key" : "HKEY_LOCAL_MACHINE\\SOFTWARE\\7-Zip","ValueName" : "Path","ValueData"   : "C:\\Program Files\\7-Zip","Ensure"  : "Present", "Hex":    false        },        {"Key" : "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Installer","ValueName" : "UWP_APP_PACKAGE_FULLNAME","ValueData" : "ReaderNotificationClient_1.0.4.0_x86__e1rzdqpraam7r","Ensure"  : "Present", "Hex":    false        }    ]}'
-    )
+    # param
+    # (
+    #     [Parameter(Mandatory=$true)]
+    #     [String]
+    #     $Registries = '{    "registries": [        {"Key" : "HKEY_LOCAL_MACHINE\\SOFTWARE\\7-Zip","ValueName" : "Path","ValueData"   : "C:\\Program Files\\7-Zip","Ensure"  : "Present", "Hex":    false        },        {"Key" : "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Installer","ValueName" : "UWP_APP_PACKAGE_FULLNAME","ValueData" : "ReaderNotificationClient_1.0.4.0_x86__e1rzdqpraam7r","Ensure"  : "Present", "Hex":    false        }    ]}'
+    # )
     
     Import-DscResource -ModuleName PSDscResources
 
-    $htRegs = $Registries | ConvertFrom-Json -AsHashtable
-
-    $i = 0
     Node localhost {
-        foreach ($reg in $htRegs["registries"])
-        {       
-    
-            Registry "RegistryAssertion_{$i.ToString()}"
-            {
-                Ensure      = $reg.Ensure
-                Key         = $reg.Key
-                ValueName   = $reg.ValueName
-                ValueData   = $reg.ValueData
-                Hex         = $reg.Hex
-            }
-
-            $i += 1
+        Registry RegistryAssertion
+        {
+            Ensure      = 'Present'
+            Key         = 'HKEY_LOCAL_MACHINE\SOFTWARE\7-Zip'
+            ValueName   = 'Path'
+            ValueData   = 'C:\Program Files\7-Zip\'
+            Hex         = $false
         }
     }
 }
 
-RegistryAssertion -OutputPath $outputPath -Verbose -Registries @"
-{
-    "registries": [
-        {
-            "Key" : "HKEY_LOCAL_MACHINE\\SOFTWARE\\7-Zip",
-            "ValueName" : "Path",
-            "ValueData"   : "C:\\Program Files\\7-Zip\\",
-            "Ensure"  : "Present",
-             "Hex":    false
-        }
-    ]
-}
-"@
+RegistryAssertion -OutputPath $outputPath -Verbose
 
 
 # param example
