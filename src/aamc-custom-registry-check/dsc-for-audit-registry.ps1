a4d196b05888d55818797ea8684e5f46b51b50df
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2023-09-26 15:20:26 +0800

    feat(dsc): updated DSC script to have parameter mandatory
    refactor: updated policy definition script to set default value with multiline Json sample

--- src/aamc-custom-registry-check/dsc-for-audit-registry.ps1
+++ src/aamc-custom-registry-check/dsc-for-audit-registry.ps1
@@ -6,7 +6,7 @@ Configuration RegistryAssertion
     # https://learn.microsoft.com/en-us/azure/governance/machine-configuration/how-to-create-policy-definition#using-parameters-in-custom-machine-configuration-policy-definitions
     param
     (
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory=$true)]
         [String]
         $Registries = '{    "registries": [        {"Key" : "HKEY_LOCAL_MACHINE\\SOFTWARE\\7-Zip","ValueName" : "Path","ValueData"   : "C:\\Program Files\\7-Zip","Ensure"  : "Present", "Hex":    false        },        {"Key" : "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Installer","ValueName" : "UWP_APP_PACKAGE_FULLNAME","ValueData" : "ReaderNotificationClient_1.0.4.0_x86__e1rzdqpraam7r","Ensure"  : "Present", "Hex":    false        }    ]}'
     )
@@ -32,7 +32,26 @@ Configuration RegistryAssertion
     }
 }
 
-RegistryAssertion -OutputPath $outputPath -Verbose
+RegistryAssertion -OutputPath $outputPath -Verbose -Registries @"
+{
+    "registries": [
+        {
+            "Key" : "HKEY_LOCAL_MACHINE\\SOFTWARE\\7-Zip",
+            "ValueName" : "Path",
+            "ValueData"   : "C:\\Program Files\\7-Zip",
+            "Ensure"  : "Present",
+             "Hex":    false
+        },
+        {
+            "Key" : "HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\Installer",
+            "ValueName" : "UWP_APP_PACKAGE_FULLNAME",
+            "ValueData" : "ReaderNotificationClient_1.0.4.0_x86__e1rzdqpraam7r",
+            "Ensure"  : "Present",
+             "Hex":    false
+        }
+    ]
+}
+"@
 
 
 # param example
